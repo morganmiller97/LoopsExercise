eb8eec2aabfb6c134da822fb06c8093c671d8aba
“morganmiller97” <adams_morgan97@icloud.com>
2022-03-18 10:52:35 -0400

    LoopsExercise

---
+++ script.js
@@ -0,0 +1,70 @@
+// LoopsExercise
+
+// 1 
+
+for (i=1; i<=7; i++){
+    console.log(i);
+  }
+
+// 2)
+for (i = 5; i <= 25; i += 4){
+  console.log(i);
+};
+
+console.log('===========');
+
+// 2)
+for (i = 5; i <= 25; i += 4){
+    console.log(i);
+};
+
+console.log('===========');
+
+// 3)
+const wizards = [
+    'Harry Potter',
+    'Hermione Granger',
+    'Ron Weasley'
+];
+for (list of wizards) {
+    console.log(list);
+};
+
+console.log('===========');
+
+// 4)
+let harryPotterMovies = 0;
+while (harryPotterMovies < 9){
+    console.log(harryPotterMovies);
+    harryPotterMovies++;
+};
+
+// 5)
+const hogwartsHouses = [
+    'Gryffindor',
+    'Hufflepuff',
+    'Ravenclaw',
+    'Slytherin'
+];
+for (list of hogwartsHouses) {
+    for (letters of list) {
+        console.log(letters);
+    }
+};
+
+console.log('===========');
+
+// 6)
+const quote = [
+    'Yer',
+    'A',
+    'Wizard',
+    'Harry'
+];
+console.log(...quote);
+
+console.log('===========');
+
+// BONUS 7
+for (i=1; i<=25; i++){
+};
